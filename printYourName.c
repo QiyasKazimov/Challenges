35932329994c15ac747275882b287678f2738973
QiyasKazimov <120410040+QiyasKazimov@users.noreply.github.com>
2022-12-13 10:15:50 +0300

    Qiyas Kazımov

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,14 @@
 // your name surname and age from user
+int main()
+{
+    char firstname[20], lastname[20];
+    int yas;
+    printf("What Is Your Firstname: ");
+    scanf("%s", firstname);
+    printf("What Is Your Lastname: ");
+    scanf("%s", lastname);
+    printf("How Old Are You: ");
+    scanf("%d", &yas);
+    printf("Your Name Is: %s\nYour Surname Is: %s\nYour Age Is: %d \n", firstname, lastname, yas);
+    return 0;
+}
